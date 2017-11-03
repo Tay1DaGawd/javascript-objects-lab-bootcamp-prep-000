13bf8e5ab6258f627eb164bdcc23dfe4d38a1a37
Tashan O'Neal <tdoneal91@gmail.com>
2017-11-03 03:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var newObj = deleteFromObjectByKey(obj, 'prop')
-  Object.assign({}, obj)
+  Object.assign({}, { prop: 1 })
   delete obj.prop
   return obj
 }
